ffe5d81ac845a0363751847da56b09eea471c012
Diego A. Suaza R <diegos9307@gmail.com>
2022-03-30 22:14:14 -0500

    implementando contador en textarea

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+
+    this.medidor = this.medidor.bind(this);
+  }
+
+  medidor(e) {
+    let arr = e.target.value.split("");
+    this.setState({
+      counter: arr.length,
+    });
+  }
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.medidor}></textarea>
+        <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
